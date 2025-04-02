bb4933e885db672a331c4dcb223e90bd8a09938d
ANIMESH <animeshtalukder7@gmail.com>
2025-04-02 12:05:58 +0530

    Add multiplication fucntion and tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,4 @@
-from calculator import add ,subtract
+from calculator import add ,subtract,multiply
 
 def test_add():
     assert add(2,3)==5
@@ -11,3 +11,10 @@ def test_subtract():
     assert subtract(0,0)==0
     assert subtract(-1,-1)==0
     assert subtract(-1,1)==-2
+    
+def test_multiply():
+    assert multiply(2,3)==6
+    assert multiply(-1,1)==-1
+    assert multiply(0,5)==0
+    assert multiply(-1,-1)==1
+    assert multiply(2,-3)==-6
